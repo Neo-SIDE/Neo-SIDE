bc280223b48a0cd28fc64836623f4e5d651f6238
everythingLeafwing <77504737+everythingLeafwing@users.noreply.github.com>
2021-03-20 08:51:43 +0100

    add program files after commenting on code

---
+++ NeoSIDE/Scripts/Formatting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+using System.IO;
+using NeoSIDE.windows;
+
+namespace NeoSIDE.formatting
+{
+    public class FormatView
+    {
+        public string stringStarter = "\"";
+        public string stringStarterAlt = "'";
+        public string multiLineStringStarter;
+
+        public string commentStarter;
+        public string multiLineCommentStarter;
+
+        public List<Keyword> keywords = new List<Keyword> {};
+    }
+
+    public class Keyword
+    {
+        public string Text = "";
+        public Color TextColor;
+
+        public Keyword(string text, Color textColor)
+        {
+            Text = text;
+            TextColor = textColor;
+        }
+    }
+}
---
+++ NeoSIDE/Scripts/Languages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using NeoSIDE.formatting;
+
+namespace NeoSIDE.Languages
+{
+    public class ScriptingLanguage
+    {
+        public FormatView formatView;
+    }
+    public class Python:ScriptingLanguage
+    {
+        public Python()
+        {
+            formatView = new FormatView() { };
+            formatView.keywords = new List<Keyword>() {
+                new Keyword("if", Colors.Red),
+                new Keyword("while", Colors.Red),
+                new Keyword("for", Colors.Red)
+            };
+        }
+    }
+}
