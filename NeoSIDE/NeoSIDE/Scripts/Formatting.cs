b625a0af7ac8f2e3ad117b1d27865f12408389e0
everythingLeafwing <77504737+everythingLeafwing@users.noreply.github.com>
2021-04-04 10:40:25 +0200

    update some things
    useless yeah yeah i know

---
+++ NeoSIDE/NeoSIDE/Scripts/Formatting.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+using System.IO;
+using NeoSIDE.windows;
+
+namespace NeoSIDE.formatting
+{
+    public class FormatView
+    {
+        public string stringStarter = "\"";
+        public string stringStarterAlt = "'";
+        public string multiLineStringStarter;
+
+        public string commentStarter;
+        public string multiLineCommentStarter;
+
+        public List<Keyword> keywords = new List<Keyword> {};
+        public List<commonError> commonErrors = new List<commonError> {};
+    }
+
+    public class Keyword
+    {
+        public string Text = "";
+        public Color TextColor;
+
+        public Keyword(string text, Color textColor)
+        {
+            Text = text;
+            TextColor = textColor;
+        }
+    }
+    public class commonError
+    {
+        public string signifier;
+
+        public commonError(string text)
+        {
+            signifier = text;
+        }
+    }
+}
